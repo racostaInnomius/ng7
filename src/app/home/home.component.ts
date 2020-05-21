5f6c1ae6ce62f43708e3e3ed5af10bd18c57c026
Rodrigo <rodrigo191@gmail.com>
2020-05-21 03:27:48 -0500

    Adding pipelines

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,6 +11,8 @@ export class HomeComponent implements OnInit {
   h1Style = false;
   users: any;
   filterUser = '';
+  home = 'HomeX';
+  birthday = new Date(1988, 3, 15);
 
   constructor(private data: DataService) { }
 
